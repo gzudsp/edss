3784212fa7099bdd3d1e12c85c191ea6c05992b7
gzulmc <546974895@qq.com>
2017-10-20 09:38:10 +0800

    22

--- src/main/java/com/work/controller/UserController.java
+++ src/main/java/com/work/controller/UserController.java
@@ -2,17 +2,21 @@ package com.work.controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -42,26 +46,20 @@ public class UserController {
 	}
 
 	@RequestMapping(value="testUpload")
-	public String testUpload(User user, MultipartFile uploadFile,Model model) {
+	@ResponseBody
+	public Message testUpload(User user, MultipartFile uploadFile,Model model,HttpServletRequest request) throws Exception {
 		User users = userService.findUserByToKenId(user.getTokenId());
 		//InputStream inputStream = uploadFile.getInputStream();
 		Message ms = new Message();
-		String description = user.getDescription();
+		/*String description = user.getDescription();
 		String name = user.getName();
-		String tokenId = user.getTokenId();
-		String filename = uploadFile.getOriginalFilename();
-		System.out.println("desc="+description+",name="+name+",tokenId="+tokenId);
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
-		Date date = new Date();
-		String formatTime = sdf.format(date);
-		//${sss}/user/服务器系统日期/zip
-		String path = "D:\\"+name+"\\"+formatTime+"\\"+filename;
-		System.out.println(path);
-		File file = new File(path);
-		System.out.println(file.getAbsolutePath());
-		boolean empty = uploadFile.isEmpty();
-		Map map = new HashMap();
-		if(empty){
+		String tokenId = user.getTokenId();*/
+		String name = request.getParameter("name");
+		String description = request.getParameter("description");
+		String tokenId = request.getParameter("tokenId");
+		
+		Map<String,String> map = new HashMap<String,String>();
+		if(uploadFile.isEmpty()){
 			System.out.println("文件未上传");
 			map.put("status", "文件未上传");
 		}else{
@@ -69,17 +67,22 @@ public class UserController {
             System.out.println("文件类型: " + uploadFile.getContentType());  
             System.out.println("文件名称: " + uploadFile.getName());  
             System.out.println("文件原名: " + uploadFile.getOriginalFilename()); */
+			String filename = uploadFile.getOriginalFilename();
+			System.out.println("desc="+description+",name="+name+",tokenId="+tokenId);
+			//${sss}/user/服务器系统日期/zip
+			String path = "D:\\"+tokenId+"\\"+filename;
+			System.out.println(path);
+			File file = new File(path);
+			System.out.println(file.getAbsolutePath());
 			if (users != null) {
 				if(!file.exists()){
 					file.mkdirs();
 				}
 				try {
 					uploadFile.transferTo(file);
-					insertData(file,name);
+					//insertData(file,name);
 					ms.setInfo("success");
 					ms.setStatus("true");
-					model.addAttribute("status", "true");
-					model.addAttribute("info", "sucess");
 				} catch (IllegalStateException | IOException  e) {
 					e.printStackTrace();
 				} catch (Exception e) {
@@ -89,11 +92,9 @@ public class UserController {
 				System.out.println("不存在该用户!");
 				ms.setInfo("不存在该用户!");
 				ms.setStatus("false");
-				model.addAttribute("status", "false");
-				model.addAttribute("info", "不存在该用户!");
 			}
 		}
-		return "result";
+		return ms;
 	}
 	
 	@RequestMapping(value="writeDataToDB",method=RequestMethod.GET)
@@ -118,10 +119,10 @@ public class UserController {
 			//数据值
 			List<String[]> list = (List<String[]>) map.get("list");
 			//删除,创建临时表
-			String newTab = rcf.checkDBTable(tabName,clumes,"userId");
-			System.out.println(newTab);
+			//String newTab = rcf.checkDBTable(tabName,clumes,"userId");
+			//System.out.println(newTab);
 			//向新表插入新数据
-			rcf.insertCustInfo(list,clumes,newTab);
+			//rcf.insertCustInfo(list,clumes,newTab);
 			ms.setInfo("success");
 			ms.setStatus("true");
 		} catch (IOException e) {
@@ -130,14 +131,13 @@ public class UserController {
 		return ms;
 	}
 	
-	public Message insertData(File zipFile,String userName){
+	public Message insertData(File zipFile,String userName) throws SQLException{
 		MyFileReader2 rcf = new MyFileReader2();
 		Message ms = new Message();
 		//path = "C:/Users/lenovo/Desktop/committee_info.zip";
 		String descDir = "A:/测试/";
 		try {
 			//解压文件
-			//List<String> files = UnZipFile.unZipFiles(new File(path), descDir);
 			List<String> files = UnZipFile.unZipFiles(zipFile, descDir);
 			//获取解压的第一个文件
 			String file = files.get(0);
@@ -147,19 +147,35 @@ public class UserController {
 			//表名
 			String tabName = (String) map.get("tabName");
 			//列集合
-			String[] clumes = (String[]) map.get("clumes");
+			List<String> clumes = (List<String>) map.get("clumes");
 			//数据值
 			List<String[]> list = (List<String[]>) map.get("list");
+			//列长度
+			List<Integer> length = (List<Integer>) map.get("lengths");
 			//删除,创建临时表
-			String newTab = rcf.checkDBTable(tabName,clumes,userName);
-			System.out.println(newTab);
-			//向新表插入新数据
-			rcf.insertCustInfo(list,clumes,newTab);
+			rcf.checkDBTable(tabName,clumes,userName,length);
+			//插入新数据
+			rcf.insertCustInfo(list,clumes,tabName);
 			ms.setInfo("success");
 			ms.setStatus("true");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		return ms;
+	}
+	
+	@RequestMapping("getFormFile")
+	@ResponseBody
+	public Message getFormFile(@RequestParam("files") MultipartFile[] files){
+		Message ms = new Message();
+		if(files!=null){
+			for (MultipartFile multipartFile : files) {
+				String filename = multipartFile.getOriginalFilename();
+				System.out.println(filename);
+			}
+			ms.setInfo("file not empty");
+		}
+		
 		return ms;
 	}
     
@@ -183,10 +199,10 @@ public class UserController {
 			String[] clumes = (String[]) map.get("clumes");
 			List<String[]> list = (List<String[]>) map.get("list");
 			//删除,创建临时表
-			String newTab = rcf.checkDBTable(tabName,clumes,"userId");
-			System.out.println(newTab);
+//			String newTab = rcf.checkDBTable(tabName,clumes,"userId");
+//			System.out.println(newTab);
 			//向新表插入新数据
-			rcf.insertCustInfo(list,clumes,newTab);
+			//rcf.insertCustInfo(list,clumes,newTab);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
